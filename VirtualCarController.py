b5fbf40bd19dfebf19c5f1d2a99f962ab6a2be62
Matheus <maicatheus@gmail.com>
2023-11-13 01:50:30 -0300

    fix control_speed and add speedometer

--- VirtualCarController.py
+++ VirtualCarController.py
@@ -10,14 +10,16 @@ class VirtualCarController:
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(max_num_hands=2)
         self.camera = cv2.VideoCapture(0)
-
+        self.speed = 0
     def normalize_value(self, value, v_min, v_max):
+        value = abs(value)
+
         if(value >= v_max):
             return 1
         if(value <= v_min):
             return 0
         
-        return (abs(value) - v_min) / (v_max - v_min)
+        return (value - v_min) / (v_max - v_min)
 
     def calculate_angle(self, p1, p2):
         delta_x = p2[0] - p1[0]
@@ -39,6 +41,7 @@ class VirtualCarController:
         thumb_distance2 = self.calculate_distance(thumb2,base_thumb2)
         
         value_to_speed_control = self.normalize_value(self.average_value([thumb_distance1,thumb_distance2]),min_value_to_speed_control,max_value_to_speed_control)
+        self.speed = value_to_speed_control
         #print(value_to_speed_control)
         if thumb_distance1 >= min_value_to_speed_control and thumb_distance2 >= min_value_to_speed_control:
             self.gamepad.left_trigger_float(0)
@@ -47,29 +50,51 @@ class VirtualCarController:
         elif (thumb_distance1 < min_value_to_speed_control and thumb_distance2 > min_value_to_speed_control) or (thumb_distance1 > min_value_to_speed_control and thumb_distance2 < min_value_to_speed_control):
             self.gamepad.right_trigger_float(0)
             self.gamepad.left_trigger_float(0.8)
+            self.speed = 0
             # print("Brake!")
         else:
             self.gamepad.right_trigger_float(0)
             self.gamepad.left_trigger_float(0)
             # print("Normal!")
 
+
     def control_steering(self, angle):
-        a_min, a_max = 1, 30
-        # print(angle)
-        if angle > a_min:
+        a_min, a_max = 0, 50
+        steerling_error = 8192
+        # multiple=1
+        # if angle > a_min and angle < 10:
+        #     multiple = 1.7
+        # if angle < a_min and angle > -10:
+        #     multiple = 1.7
+            
+        if angle >= a_min:
             if angle > a_max:
                 angle = a_max
-            self.gamepad.left_joystick_float(self.normalize_value(angle, a_min, a_max), 0)
+            self.gamepad.left_joystick(int(self.normalize_value(angle, a_min, a_max)*32768 + steerling_error), 0)
             # print("Right")
         elif angle < -a_min:
             if angle < -a_max:
                 angle = -a_max
-            self.gamepad.left_joystick_float(-self.normalize_value(angle, a_min, a_max), 0)
+            self.gamepad.left_joystick(int(-self.normalize_value(angle, a_min, a_max)*32768 - steerling_error), 0)
             # print("Left")
         else:
-            self.gamepad.left_joystick_float(0, 0)
+            self.gamepad.left_joystick(0, 0)
             # print("Forward")
-   
+    
+    def draw_speedometer(self, frame):
+        bar_x = 50  
+        bar_y = 100 
+        bar_height = 300 
+        bar_width = 20  
+
+        cv2.rectangle(frame, (bar_x, bar_y), (bar_x + bar_width, bar_y + bar_height), (255, 255, 255), 2)
+
+        filled_height = int(bar_height * self.speed)
+
+        cv2.rectangle(frame, (bar_x, bar_y + bar_height - filled_height), (bar_x + bar_width, bar_y + bar_height), (0, 255, 0), cv2.FILLED)
+
+        cv2.putText(frame, f"Speed: {int(self.speed * 100)}", (bar_x - 40, bar_y + bar_height + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
 
     def run(self):
         while True:
@@ -100,8 +125,9 @@ class VirtualCarController:
                         self.control_steering(angle)
                         self.control_speed(double_hands[0], double_hands[1],thumb1,thumb2,base_thumb1,base_thumb2)
 
-                        self.gamepad.update()
+                        self.draw_speedometer(frame)
 
+                        self.gamepad.update()
                         cv2.line(frame, double_hands[0][0], double_hands[1][0], (255, 0, 0), 3)
                         cv2.line(frame, thumb1, base_thumb1, (0, 255, 0), 2)
                         cv2.line(frame, thumb2, base_thumb2, (0, 255, 0), 2)

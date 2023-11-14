b7f5cf6ebeee127df488214c54e506d2d47a5dc6
Matheus <maicatheus@gmail.com>
2023-11-14 14:44:46 -0300

    adding accelerator and brake to each thumb

--- VirtualCarController.py
+++ VirtualCarController.py
@@ -11,6 +11,8 @@ class VirtualCarController:
         self.hands = self.mpHands.Hands(max_num_hands=2)
         self.camera = cv2.VideoCapture(0)
         self.speed = 0
+        self.brake = 0
+
     def normalize_value(self, value, v_min, v_max):
         value = abs(value)
 
@@ -33,25 +35,21 @@ class VirtualCarController:
     def average_value(self, values):
         return sum(values)/len(values)
     
-    def control_speed(self, hand1, hand2,thumb1,thumb2,base_thumb1, base_thumb2):
-        max_value_to_speed_control = 100
+    def control_speed(self,thumb1,thumb2,base_thumb1, base_thumb2):
+        max_value_to_speed_control = 80
         min_value_to_speed_control = 30
 
         thumb_distance1 = self.calculate_distance(thumb1,base_thumb1)
         thumb_distance2 = self.calculate_distance(thumb2,base_thumb2)
         
-        value_to_speed_control = self.normalize_value(self.average_value([thumb_distance1,thumb_distance2]),min_value_to_speed_control,max_value_to_speed_control)
-        self.speed = value_to_speed_control
+        self.speed = self.normalize_value(thumb_distance1,min_value_to_speed_control,max_value_to_speed_control)
+        self.brake = self.normalize_value(thumb_distance2,min_value_to_speed_control,max_value_to_speed_control)
         #print(value_to_speed_control)
-        if thumb_distance1 >= min_value_to_speed_control and thumb_distance2 >= min_value_to_speed_control:
-            self.gamepad.left_trigger_float(0)
-            self.gamepad.right_trigger_float(value_to_speed_control)
-            # print("speeds up!")
-        elif (thumb_distance1 < min_value_to_speed_control and thumb_distance2 > min_value_to_speed_control) or (thumb_distance1 > min_value_to_speed_control and thumb_distance2 < min_value_to_speed_control):
-            self.gamepad.right_trigger_float(0)
-            self.gamepad.left_trigger_float(0.8)
-            self.speed = 0
+        if thumb_distance1 >= min_value_to_speed_control or thumb_distance2 >= min_value_to_speed_control:
+            self.gamepad.left_trigger_float(self.brake*1.4)
             # print("Brake!")
+            self.gamepad.right_trigger_float(self.speed)
+            # print("speeds up!")
         else:
             self.gamepad.right_trigger_float(0)
             self.gamepad.left_trigger_float(0)
@@ -81,19 +79,19 @@ class VirtualCarController:
             self.gamepad.left_joystick(0, 0)
             # print("Forward")
     
-    def draw_speedometer(self, frame):
-        bar_x = 50  
+    def draw_rectangle_of_speed(self, frame,text,mode,color, x_position):
+        bar_x = x_position  
         bar_y = 100 
         bar_height = 300 
         bar_width = 20  
 
         cv2.rectangle(frame, (bar_x, bar_y), (bar_x + bar_width, bar_y + bar_height), (255, 255, 255), 2)
 
-        filled_height = int(bar_height * self.speed)
+        filled_height = int(bar_height * mode)
 
-        cv2.rectangle(frame, (bar_x, bar_y + bar_height - filled_height), (bar_x + bar_width, bar_y + bar_height), (0, 255, 0), cv2.FILLED)
+        cv2.rectangle(frame, (bar_x, bar_y + bar_height - filled_height), (bar_x + bar_width, bar_y + bar_height), color, cv2.FILLED)
 
-        cv2.putText(frame, f"Speed: {int(self.speed * 100)}", (bar_x - 40, bar_y + bar_height + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(frame, f"{text}: {int(mode * 100)}", (bar_x - 40, bar_y + bar_height + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 
     def run(self):
@@ -121,18 +119,24 @@ class VirtualCarController:
                         base_thumb2 = double_hands[1][6]
 
                         angle = self.calculate_angle(double_hands[0][0], double_hands[1][0])
-                        cv2.putText(frame, f"Angle: {int(angle)}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                        cv2.putText(frame, f"Angle: {int(angle)}", (100, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
                         self.control_steering(angle)
-                        self.control_speed(double_hands[0], double_hands[1],thumb1,thumb2,base_thumb1,base_thumb2)
+                        self.control_speed(thumb1,thumb2,base_thumb1,base_thumb2)
 
-                        self.draw_speedometer(frame)
+                        self.draw_rectangle_of_speed(frame,"Speed", self.speed,(0,255,0),500)
+                        self.draw_rectangle_of_speed(frame,"Brake", self.brake,(0,0,255),100)
 
                         self.gamepad.update()
                         cv2.line(frame, double_hands[0][0], double_hands[1][0], (255, 0, 0), 3)
                         cv2.line(frame, thumb1, base_thumb1, (0, 255, 0), 2)
-                        cv2.line(frame, thumb2, base_thumb2, (0, 255, 0), 2)
+                        cv2.line(frame, thumb2, base_thumb2, (0, 0, 255), 2)
                     except Exception as e:
-                          print(f"Error processing landmarks: {e}")
+                        self.speed = 0
+                        self.brake = 0
+                        self.gamepad.left_joystick(0, 0)
+                        self.gamepad.update()
+
+                        print(f"Error processing landmarks: {e}")
 
                 cv2.imshow("Virtual driving control", frame)
             except:
